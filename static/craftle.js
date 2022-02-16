7626d6a254086fb30cb17682fc34a1fab10f4c40
rasmus <mail@rasmusantons.de>
2022-02-17 00:03:07 +0100

    shapeless recipes can be crafted

--- static/craftle.js
+++ static/craftle.js
@@ -1,6 +1,9 @@
 let recipes;
 let targetRecipe;
 let ingredients;
+let selectedIngredient = null;
+let craftingInputs;
+let craftingOutput;
 
 function initRandom() {
 	targetRecipe = recipes[Math.floor(Math.random() * recipes.length)];
@@ -16,7 +19,73 @@ function initIngredients() {
 		ingredientDiv.classList.add('ingredient');
 		ingredientDiv.style.backgroundImage = `url("/img/${ingredientId}.png")`;
 		ingredientsDiv.appendChild(ingredientDiv);
+		ingredientDiv.addEventListener('click', () => {
+			selectedIngredient = ingredient;
+		});
 	}
+}
+
+function initCraftingTable() {
+	craftingInputs = new Array(9).fill(null);
+	craftingOutput = null;
+	for (let [i, ingredientInput] of Object.entries(document.querySelectorAll('#crafting-input .ingredient'))) {
+		ingredientInput.addEventListener('click', () => {
+			craftingInputs[i] = selectedIngredient;
+			if (selectedIngredient) {
+				let ingredientId = selectedIngredient.replace(':', '/');
+				ingredientInput.style.backgroundImage = `url("/img/${ingredientId}.png")`;
+			} else {
+				ingredientInput.style.backgroundImage = '';
+			}
+			selectedIngredient = null;
+			updateCraftingOutput();
+		});
+	}
+}
+
+function updateCraftingOutput() {
+	const craftingOutputDiv = document.getElementById('crafting-output');
+	for (let recipe of recipes) {
+		if (checkExactRecipe(recipe)) {
+			craftingOutput = recipe.result.item;
+			let ingredientId = craftingOutput.replace(':', '/');
+			craftingOutputDiv.style.backgroundImage = `url("/img/${ingredientId}.png")`;
+			return;
+		}
+	}
+	craftingOutput = null;
+	craftingOutputDiv.style.backgroundImage = '';
+}
+
+function checkExactRecipe(recipe) {
+	if (recipe.type === 'minecraft:crafting_shapeless') {
+		const remainingInputs = [...craftingInputs];
+		for (let recipeIngredient of recipe.ingredients) {
+			if (Array.isArray(recipeIngredient)) {
+				let found = false;
+				for (let ingredientChoice of recipeIngredient) {
+					if (remainingInputs.includes(ingredientChoice.tag || ingredientChoice.item)) {
+						remainingInputs[remainingInputs.indexOf(ingredientChoice.tag || ingredientChoice.item)] = null;
+						found = true;
+						break;
+					}
+				}
+				if (!found)
+					return false;
+			} else {
+				if (remainingInputs.includes(recipeIngredient.tag || recipeIngredient.item)) {
+					remainingInputs[remainingInputs.indexOf(recipeIngredient.tag || recipeIngredient.item)] = null;
+				} else {
+					return false;
+				}
+			}
+		}
+		for (let remainingInput of remainingInputs)
+			if (remainingInput !== null)
+				return false;
+		return true;
+	}
+	return false;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -42,6 +111,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 		ingredients = Array.from(ingredients).sort();
 		initIngredients();
+		initCraftingTable();
 		document.getElementById('start-random').addEventListener('click', initRandom);
 	});
 });
