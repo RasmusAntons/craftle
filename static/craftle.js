350736d3d9544a4f5a28358e149e2afed6af990b
rasmus <mail@rasmusantons.de>
2022-02-21 00:00:40 +0100

    fix indentation

--- static/craftle.js
+++ static/craftle.js
@@ -13,12 +13,12 @@ let cursorItemDiv;
 const MAX_ATTEMPTS = 36;
 
 function mulberry32(a) {
-    return function() {
+	return function() {
 		let t = a += 0x6D2B79F5;
 		t = Math.imul(t ^ t >>> 15, t | 1);
 		t ^= t + Math.imul(t ^ t >>> 7, t | 61);
 		return ((t ^ t >>> 14) >>> 0) / 4294967296;
-    }
+	}
 }
 
 function initDaily() {
@@ -360,7 +360,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 	document.addEventListener('mousedown', e => {
 		if (e.button !== 0)
-				return;
+			return;
 		if (e.target === document.body || e.target.id === 'ingredients') {
 			selectedIngredient = null;
 			setIngredientInput(cursorItemDiv, selectedIngredient);
