7572a9e536627e3644f8ca07977afeb268af089c
rasmus <mail@rasmusantons.de>
2022-02-20 22:28:34 +0100

    implement crafting feedback

--- static/craftle.js
+++ static/craftle.js
@@ -73,7 +73,7 @@ function initCraftingTable() {
 	const craftingOutputDiv = document.getElementById('crafting-output');
 	craftingOutputDiv.addEventListener('click', () => {
 		if (craftingOutput !== null) {
-			const inventoryDiv = document.querySelectorAll('#crafting-inventory .ingredient')[attempts];
+			const inventoryDiv = document.querySelectorAll('.invslot .ingredient')[attempts];
 			setIngredientInput(inventoryDiv, craftingOutput);
 			handleCraftingAttempt();
 		}
@@ -103,6 +103,11 @@ function handleCraftingAttempt() {
 		alert('success');
 	} else if (attempts === MAX_ATTEMPTS) {
 		alert('fail');
+	} else {
+		const craftingFeedback = getCraftingFeedback();
+		for (let [i, craftSlot] of Object.entries(document.querySelectorAll('#crafting-input .craftslot'))) {
+			craftSlot.style.backgroundColor = craftingFeedback[i] ? craftingFeedback[i] : '';
+		}
 	}
 }
 
@@ -210,8 +215,74 @@ function checkExactRecipe(recipe) {
 	return false;
 }
 
+/*
+returns [score, colours]
+where score = 100 * green
+ */
 function scoreShapelessRecipe(recipe) {
+	let expectedIngredients = new Array(9).fill(null);
+	let resultColours = new Array(9).fill(null);
+	let score = 0;
+	for (let [i, expectedIngredientChoices] of recipe.ingredients.entries())
+		expectedIngredients[i] = expandIngredientChoices(expectedIngredientChoices);
+	for (let [inputSlot, craftingInput] of craftingInputs.entries()) {
+		let matchingIngredientIdx = -1;
+		for (let [ingredientIdx, expectedIngredientChoices] of expectedIngredients.entries()) {
+			const bothNull = craftingInput == null && expectedIngredientChoices == null;
+			const compatible = expectedIngredientChoices !== null && expectedIngredientChoices.includes(craftingInput);
+			if (bothNull || compatible) {
+				matchingIngredientIdx = ingredientIdx;
+				break;
+			}
+		}
+		if (matchingIngredientIdx >= 0) {
+			resultColours[inputSlot] = 'green';
+			expectedIngredients.splice(matchingIngredientIdx, 1);
+			score += 100;
+		}
+	}
+	return [score, resultColours];
+}
 
+/*
+returns [score, colours]
+where score = 100 * green + 10 * yellow - 3 * rowOffset - collOffset
+ */
+function scoreShapedRecipe(recipe, rowOffset, colOffset) {
+	let expectedIngredients = flattenCraftingPattern(recipe.pattern, rowOffset, colOffset).map(e =>
+		(e === null) ? null : expandIngredientChoices(recipe.key[e]));
+	let resultColours = new Array(9).fill(null);
+	let score = -3 * rowOffset - colOffset;
+	for (let [inputSlot, craftingInput] of craftingInputs.entries()) {
+		const bothNull = craftingInput == null && expectedIngredients[inputSlot] == null;
+		const compatible = expectedIngredients[inputSlot] !== null && expectedIngredients[inputSlot].includes(craftingInput);
+		if (bothNull || compatible) {
+			resultColours[inputSlot] = 'green';
+			expectedIngredients[inputSlot] = undefined;
+			score += 100;
+		}
+	}
+	for (let [inputSlot, craftingInput] of craftingInputs.entries()) {
+		if (expectedIngredients[inputSlot] === undefined)
+			continue;
+		let matchingIngredientIdx = -1;
+		for (let [ingredientIdx, expectedIngredientChoices] of expectedIngredients.entries()) {
+			if (expectedIngredientChoices === undefined)
+				continue;
+			const bothNull = craftingInput == null && expectedIngredientChoices == null;
+			const compatible = expectedIngredientChoices !== null && expectedIngredientChoices.includes(craftingInput);
+			if (bothNull || compatible) {
+				matchingIngredientIdx = ingredientIdx;
+				break;
+			}
+		}
+		if (matchingIngredientIdx >= 0) {
+			resultColours[inputSlot] = 'yellow';
+			expectedIngredients[inputSlot] = undefined;
+			score += 10;
+		}
+	}
+	return [score, resultColours];
 }
 
 function getCraftingFeedback() {
@@ -220,11 +291,17 @@ function getCraftingFeedback() {
 		if (recipe.type === 'minecraft:crafting_shapeless') {
 			scoredFeedbackOptions.push(scoreShapelessRecipe(recipe));
 		} else if (recipe.type === 'minecraft:crafting_shaped') {
-			// scoredFeedbackOptions.push(scoreShapelessRecipe(recipe));
+			let nRows = recipe.pattern.length;
+			let nCols = Math.max(...recipe.pattern.map(e => e.length));
+			for (let rowOffset = 0; rowOffset < (4 - nRows); ++rowOffset) {
+				for (let colOffset = 0; colOffset < (4 - nCols); ++colOffset) {
+					scoredFeedbackOptions.push(scoreShapedRecipe(recipe, rowOffset, colOffset));
+				}
+			}
 		}
 	}
-	scoredFeedbackOptions.sort((a, b) => a[0] > b[0]);
-	return scoredFeedbackOptions[0];
+	scoredFeedbackOptions.sort((a, b) => a[0] < b[0]);
+	return scoredFeedbackOptions[0][1];
 }
 
 document.addEventListener('DOMContentLoaded', () => {
